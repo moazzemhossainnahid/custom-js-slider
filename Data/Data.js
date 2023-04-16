f80e14c4b38725602cecc9b965d8724dc3c9e3b2
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-04-16 17:58:24 +0600

    add car

---
+++ Data/Data.js
@@ -0,0 +1,27 @@
+export const Data = [
+    {
+        id:1,
+        title:"Audi TT",
+        img:"https://www.pngplay.com/wp-content/uploads/15/Audi-Tt-PNG-Free-File-Download.webp"
+    },
+    {
+        id:2,
+        title:"BMW M3 F30",
+        img:"https://www.pngmart.com/files/3/BMW-M3-PNG-File.png"
+    },
+    {
+        id:3,
+        title:"ASTON MARTIN DB11",
+        img:"https://pngimg.com/uploads/aston_martin/aston_martin_PNG9.png"
+    },
+    {
+        id:4,
+        title:"TOYOTA AQUA",
+        img:"https://www.motortrend.com/uploads/sites/10/2020/12/2021-toyota-prius-xle-hybrid-5door-hatchback-angular-front.png"
+    },
+    {
+        id:5,
+        title:"HONDA VEZEL",
+        img:"https://www.popularcar.com/wp-content/uploads/2017/10/honda-vezel.png"
+    },
+]
